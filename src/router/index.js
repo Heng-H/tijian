6b736336ee82c655b97c7848880cbe259dcf7b95
何杰鸣 <9759400+he-jieming@user.noreply.gitee.com>
2024-06-13 21:41:53 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,10 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: Register
+    },{
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
