4ca41ac8835923c70e83ea7d66c1e26d2c2af877
何杰鸣 <9759400+he-jieming@user.noreply.gitee.com>
2024-06-20 14:05:18 +0800

    增加修改密码功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -22,16 +22,18 @@
                 <i class="fa fa-smile-o" aria-hidden="true"></i>
                 <input type="text" v-model="users.code" placeholder="输入验证码">
                 <span class="time" v-if="isCountingDown">{{ countdown }} 秒后重新发送</span>
-                <button class="send_code" v-else @click="startCountdown">发送验证码</button>
+                <button class="send_code" v-else @click="send">发送验证码</button>
             </div>
 
 
             <div class="reg-box">
                 <p @click="toRegister">注册</p>
-                <p>忘记密码？</p>
+                <p @click="dialogFormVisible = true">忘记密码？</p>
+
             </div>
             <div class="button-box">
-                <el-button v-if="loginMethod === 'password'" class="button-box" :plain="true" @click="login">登录</el-button>
+                <el-button v-if="loginMethod === 'password'" class="button-box" :plain="true"
+                    @click="login">登录</el-button>
                 <el-button v-else class="button-box" :plain="true" @click="codeLogin">登录</el-button>
             </div>
         </section>
@@ -43,6 +45,35 @@
             </div>
             <p>4008-XXX-XXX</p>
         </footer>
+
+        <el-dialog v-model="dialogFormVisible" title="忘记密码" width="300">
+            <el-form :model="form">
+                <el-form-item label="手机号码：" label-width="100px">
+                    <el-input v-model="form.phone" autocomplete="off" />
+                </el-form-item>
+                <el-form-item v-if="step === 2" label="验证码：" label-width="100px">
+                    <el-input v-model="form.code" autocomplete="off" />
+                    <span class="time" v-if="isCountingDown1">{{ countdown1 }} 秒后重新发送</span>
+                    <!-- <button type="button" class="send_code" v-else @click="sendCode">发送验证码</button> -->
+                </el-form-item>
+                <span v-else-if="step === 3">
+                    <el-form-item label="密码：" label-width="100px">
+                        <el-input type="password" v-model="form.password" autocomplete="off" />
+                    </el-form-item>
+                    <el-form-item label="确认密码：" label-width="100px">
+                        <el-input type="password" v-model="form.repassword" autocomplete="off" />
+                    </el-form-item>
+                </span>
+                <el-form-item>
+                    <el-button type="primary" @click="canel">取 消</el-button>
+                    <el-button v-if="step==1" type="primary" @click="sendCode">发送验证码</el-button>
+                    <el-button v-else-if="step == 2" type="primary" @click="next">下一步</el-button>
+                    <el-button v-else-if="step==3" type="primary" @click="update">确 定</el-button>
+                   
+                </el-form-item>
+            </el-form>
+
+        </el-dialog>
     </div>
 </template>
 
@@ -62,11 +93,152 @@ const users = reactive({
     password: "",
     code: ""
 });
+//忘记密码提示框
+const dialogFormVisible = ref(false);
+//忘记密码表单
+const form = reactive({
+    phone: '',
+    code: '',
+    password: '',
+    repassword: ''
+});
+//忘记密码步骤
+const step = ref(1);
 
+//取消忘记密码的操作
+const canel = () => {
+    dialogFormVisible.value = false;
+    step.value = 1;
+    form.phone = "";
+    form.code = "";
+    form.password = "";
+    form.repassword = "";
+}
+//下一步，验证手机号和验证码
+const next = () => {
+    if (form.phone == "") {
+        ElMessage.error("请输入手机号");
+        return;
+    }
+    if (form.code == "") {
+        ElMessage.error("请输入验证码");
+        return;
+    }
+
+    axios({
+        method: "post",
+        url: "/api/users/loginByCode",
+        data: {
+            phone: form.phone,
+            code: form.code
+        },
+        headers: 'Content-Type:application/json'
+    })
+        .then(res => {
+            if (res.data.code == 1) {
+                ElMessage({
+                    message: "验证成功",
+                    type: "success"
+                })
+                step.value = 2;
+            } else {
+                ElMessage.error(res.data.message);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+}
+//修改密码
+const update = () => {
+    if(form.phone==""){
+        ElMessage.error("请输入手机号");
+        return;
+    }
+    if(form.password==""){
+        ElMessage.error("请输入密码");
+        return;
+    }
+    if(form.repassword==""){
+        ElMessage.error("请输入确认密码");
+        return;
+    }
+    if(form.password!=form.repassword){
+        ElMessage.error("两次密码输入不一致");
+        return;
+    }
+
+    axios({
+        method: "post",
+        url: "/api/users/updatePassword",
+        data: {
+            phone: form.phone,
+            password: form.password
+        },
+        headers: 'Content-Type:application/json'
+    })
+        .then(res => {
+            if (res.data.code == 1) {
+                ElMessage({
+                    message: "修改成功",
+                    type: "success"
+                })
+                dialogFormVisible.value = false;
+                step.value = 1;
+            } else {
+                ElMessage.error(res.data.message);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+}
+
+//是否在倒计时
 const isCountingDown = ref(false);
-const countdown = ref('60');
+const isCountingDown1 = ref(false);
+//倒计时
+const countdown = ref('30');
+const countdown1 = ref('30');
 
-const send=()=>{
+//忘记密码的发送验证码
+const sendCode = () => {
+
+    if(form.phone==""){
+        console.log(form.phone)
+        ElMessage.error("请输入手机号");
+        return;
+    }
+    axios({
+        method: "get",
+        url: "/api/users/sendCode",
+        params: {
+            phone: form.phone
+        },
+    })
+        .then(res => {
+            if (res.data.code == 1) {
+                ElMessage({
+                    message: "发送成功",
+                    type: "success"
+                })
+                startCountdown1();
+                step.value=2;
+            } else {
+                ElMessage.error(res.data.message);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+}
+
+//首界面登录的发送验证码
+const send = () => {
+    if (users.userId == "") {
+        ElMessage("请输入手机号");
+        return;
+    }
     axios({
         method: "get",
         url: "/api/users/sendCode",
@@ -80,6 +252,7 @@ const send=()=>{
                     message: "发送成功",
                     type: "success"
                 })
+                startCountdown();
             } else {
                 ElMessage.error(res.data.message);
             }
@@ -89,6 +262,7 @@ const send=()=>{
         })
 }
 
+//发送验证码的倒计时
 const startCountdown = () => {
     if (isCountingDown.value) return; // 如果已经在倒计时中，则不做任何操作
     isCountingDown.value = true;
@@ -99,10 +273,22 @@ const startCountdown = () => {
         } else {
             isCountingDown.value = false; // 倒计时结束
             clearInterval(interval); // 清除定时器
-
         }
     }, 1000);
-    send();
+}
+
+const startCountdown1 = () => {
+    if (isCountingDown1.value) return; // 如果已经在倒计时中，则不做任何操作
+    isCountingDown1.value = true;
+    countdown1.value = '30'; // 重置倒计时秒数
+    setInterval(() => {
+        if (countdown1.value > 0) {
+            countdown1.value -= 1;
+        } else {
+            isCountingDown1.value = false; // 倒计时结束
+            clearInterval(interval); // 清除定时器
+        }
+    }, 1000);
 }
 
 
@@ -110,6 +296,8 @@ const toggleLoginMethod = () => {
     console.log(loginMethod.value);
     loginMethod.value = loginMethod.value === 'password' ? 'code' : 'password';
 }
+
+//
 const login = () => {
     if (users.userId == "") {
         ElMessage("请输入手机号");
@@ -151,6 +339,7 @@ const login = () => {
         })
 }
 
+//验证码登录
 const codeLogin = () => {
     if (users.userId == "") {
         ElMessage("请输入手机号");
@@ -282,14 +471,15 @@ section .button-box {
     cursor: pointer;
 }
 
-.send_code{
+.send_code {
     margin-right: 1vw;
     font-size: 3.3vw;
     cursor: pointer;
     color: #409EFF;
     padding: 0 0.1vw;
 }
-.time{
+
+.time {
     margin-right: 1vw;
     font-size: 3.3vw;
     cursor: pointer;
